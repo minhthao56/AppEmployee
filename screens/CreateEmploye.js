637f332488c8fd1078ca40b24ac28d6be77d4da6
minhthao56 <minthao5648@gmail.com>
2020-08-18 14:16:12 +0700

    add create employe

---
+++ screens/CreateEmploye.js
@@ -0,0 +1,127 @@
+import React, {useState} from 'react';
+import {
+  View,
+  TextInput,
+  Button,
+  Modal,
+  StyleSheet,
+  Alert,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
+
+export default function CreateEmploye() {
+  const [valueName, onChangeName] = useState('');
+  const [valueEmail, onChangeEmail] = useState('');
+  const [valuePhone, onChangePhone] = useState('');
+  const [valueSalary, onChangeSalary] = useState('');
+  const [isShowModal, setIsShowModal] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <Modal
+        animationType="slide"
+        visible={isShowModal}
+        onRequestClose={() => Alert.alert('Close')}
+        style={styles.modal}>
+        <View>
+          <Text>Ok nha</Text>
+
+          <TouchableOpacity
+            style={styles.containerLose}
+            activeOpacity={0.5}
+            onPress={() => setIsShowModal(!isShowModal)}>
+            <Text style={styles.buttonLose}>LOSE</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+      <TextInput
+        onChangeText={(text) => onChangeName(text)}
+        style={styles.input}
+        value={valueName}
+        placeholder="Name"
+        textContentType="name"
+      />
+      <TextInput
+        onChangeText={(text) => onChangeEmail(text)}
+        style={styles.input}
+        value={valueEmail}
+        placeholder="Email"
+        textContentType="emailAddress"
+        keyboardType="email-address"
+      />
+      <TextInput
+        onChangeText={(text) => onChangePhone(text)}
+        style={styles.input}
+        value={valuePhone}
+        placeholder="Phone"
+        keyboardType="number-pad"
+        textContentType="telephoneNumber"
+      />
+      <TextInput
+        onChangeText={(text) => onChangeSalary(text)}
+        style={styles.input}
+        value={valueSalary}
+        placeholder="Salary"
+        keyboardType="number-pad"
+      />
+      <View style={styles.containerButton}>
+        <Icon.Button
+          name="up"
+          size={30}
+          color="white"
+          backgroundColor="'rgba(255, 0, 255, 0)'"
+          iconStyle={{marginLeft: 'auto', marginRight: 'auto'}}
+          style={styles.buttonUp}
+          onPress={() => setIsShowModal(!isShowModal)}
+        />
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 8,
+    marginRight: 8,
+    marginTop: 8,
+  },
+  input: {
+    height: 40,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    marginTop: 8,
+  },
+  buttonUp: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'red',
+    borderRadius: 15,
+  },
+  containerButton: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 8,
+  },
+  buttonLose: {
+    backgroundColor: 'red',
+    width: 50,
+    height: 40,
+    borderRadius: 50,
+    color: 'white',
+  },
+  containerLose: {
+    textAlign: 'center',
+  },
+});
--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,87 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, StatusBar, FlatList} from 'react-native';
+import ListIterm from '../components/ListIterm';
 
 export default function Home() {
+  const DATA = [
+    {
+      id: 1,
+      name: 'First Item',
+      title: 'Web Derveloper',
+      url: 'http://kundenarea.at/app-assets/images/user/12.jpg',
+    },
+    {
+      id: 2,
+      name: 'Second Item',
+      title: 'Front End Derveloper',
+      url: 'http://kundenarea.at/app-assets/images/user/12.jpg',
+    },
+    {
+      id: 3,
+      name: 'Three Item',
+      title: 'Back End Derveloper',
+      url: 'http://kundenarea.at/app-assets/images/user/12.jpg',
+    },
+    {
+      id: 4,
+      name: 'Four Item',
+      title: 'Full stack Derveloper',
+      url: 'http://kundenarea.at/app-assets/images/user/12.jpg',
+    },
+    {
+      id: 5,
+      name: 'Five Item',
+      title: 'MERN Stack Derveloper',
+      url: 'http://kundenarea.at/app-assets/images/user/12.jpg',
+    },
+    {
+      id: 6,
+      name: 'Five Item',
+      title: 'MERN Stack Derveloper',
+      url: 'http://kundenarea.at/app-assets/images/user/12.jpg',
+    },
+    {
+      id: 7,
+      name: 'Five Item',
+      title: 'MERN Stack Derveloper',
+      url: 'http://kundenarea.at/app-assets/images/user/12.jpg',
+    },
+    {
+      id: 8,
+      name: 'Five Item',
+      title: 'MERN Stack Derveloper',
+      url: 'http://kundenarea.at/app-assets/images/user/12.jpg',
+    },
+    {
+      id: 9,
+      name: 'Five Item',
+      title: 'MERN Stack Derveloper',
+      url: 'http://kundenarea.at/app-assets/images/user/12.jpg',
+    },
+    {
+      id: 10,
+      name: 'Five Item',
+      title: 'MERN Stack Derveloper',
+      url: 'http://kundenarea.at/app-assets/images/user/12.jpg',
+    },
+  ];
+
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.safeArea}>
+        <FlatList
+          data={DATA}
+          renderItem={({item}) => <ListIterm item={item} />}
+          keyExtractor={(item) => item.id}
+          contentContainerStyle={{paddingLeft: 8, paddingRight: 8}}
+        />
+      </SafeAreaView>
+    </>
   );
 }
+const styles = StyleSheet.create({
+  safeArea: {
+    backgroundColor: '#dddddd',
+  },
+});
